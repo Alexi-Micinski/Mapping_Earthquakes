5d9bb7126520ade99d22e5649e5b84779fc248df
Alexi-Micinski <106785377+Alexi-Micinski@users.noreply.github.com>
2022-10-10 19:45:39 -0600

    update polygons

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,43 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
-// // Add GeoJSON data.
-// let sanFranAirport = {
-//     "type": "FeatureCollection",
-//     "features": [{
-//         "type": "Feature",
-//         "properties": {
-//             "id": "3469",
-//             "name": "San Francisco International Airport",
-//             "city": "San Francisco",
-//             "country": "United States",
-//             "faa": "SFO",
-//             "icao": "KSFO",
-//             "alt": "13",
-//             "tz-offset": "-8",
-//             "dst": "A",
-//             "tz": "America/Los_Angeles"
-//         },
-//         "geometry": {
-//             "type": "Point",
-//             "coordinates": [-122.375, 37.61899948120117]
-//         }
-//     }]
-// };
-
-// // Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     onEachFeature: function(feature, layer) {
-//         console.log(feature);
-//         layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
-//     }
-
-// }).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -51,20 +14,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     accessToken: API_KEY
 });
 
-// // We create the light view tile layer that will be an option for our map.
-// let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-// // We create the dark view tile layer that will be an option for our map.
-// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
@@ -77,18 +26,6 @@ let map = L.map("mapid", {
     zoom: 11,
     layers: [satelliteStreets]
 });
-
-// // Pass our map layers into our layers control and add the layers control to the map.
-// L.control.layers(baseMaps).addTo(map);
-
-// // Then we add our 'graymap' tile layer to the map.
-// light.addTo(map);
-
-// // Accessing the airport GeoJSON URL
-// let airportData = "https://raw.githubusercontent.com/Alexi-Micinski/Mapping_Earthquakes/main/majorAirports.json";
-
-// // Accessing the Toronto airline routes GeoJSON URL.
-// let torontoData = "https://raw.githubusercontent.com/Alexi-Micinski/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/Alexi-Micinski/Mapping_Earthquakes/main/torontoNeighborhoods.json";
@@ -111,9 +48,3 @@ d3.json(torontoHoods).then(function(data) {
         }
     }).addTo(map);
 });
-
-// d3.json(torontoHoods).then(function(data) {
-//     console.log(data);
-//     // Creating a GeoJSON layer with the retrieved data.
-//     L.geoJSON(data).addTo(map);
-// });
